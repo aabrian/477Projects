b6b001c13f04f9d9e17ebf02a3ec7e514fa1daeb
David <daderinw@umd.edu>
2023-03-07 10:46:47 -0500

    hard code z

--- line_test.py
+++ line_test.py
@@ -106,6 +106,8 @@ if __name__ == '__main__':
                     theta = (np.arctan2(mag_cross, dot_AB))*180/np.pi
                     # print("theta: ", theta)
                 kt = 1
+                if theta < 0:
+                    z = 0
                 ep_chassis.drive_speed(x = v_b[1], y = v_b[0], z=kt*theta, timeout=.5)
 
             cv2.imshow("img", img)
