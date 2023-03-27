64c549a9ce9eee818df7c004b63b4bfca5832b19
Brian Amaya <brianamaya3605@gmail.com>
2023-03-27 16:10:25 -0400

    jh

--- Proj2/ModelTest.py
+++ Proj2/ModelTest.py
@@ -3,19 +3,11 @@ import cv2
 rf = Roboflow(api_key="kKusTXhj0ObVGmi9slHp")
 project = rf.workspace().project("project2-l7rdy")
 model = project.version(4).model
-
-vid = cv2.VideoCapture(0)
-
-while True:
-    ret, frame = vid.read()
-    # frame = ep_camera.read_cv2_image(strategy="newest", timeout=0.5)
-
-
-    # infer on a local image
-    model.predict(frame)
+print(model)
+# infer on a local image
+print(model.predict("Proj2\img1.png", confidence=40, overlap=30).json())
 
 # visualize your prediction
-# model.predict("your_image.jpg", confidence=40, overlap=30).save("prediction.jpg")
-
+model.predict("Proj2\img1.png", confidence=40, overlap=30).save("prediction.jpg")
 # infer on an image hosted elsewhere
 # print(model.predict("URL_OF_YOUR_IMAGE", hosted=True, confidence=40, overlap=30).json())
