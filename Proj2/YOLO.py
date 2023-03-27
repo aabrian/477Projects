efbf329c8a5a8650f4cbcb200c5386a4bcb87180
Brian Amaya <brianamaya3605@gmail.com>
2023-03-27 14:18:41 -0400

    fixed

--- Proj2/YOLO.py
+++ Proj2/YOLO.py
@@ -9,18 +9,18 @@ model = YOLO("yolov8n.pt")
 
 
 # Use vid instead of ep_camera to use your laptop's webcam
-# vid = cv2.VideoCapture(0)
+vid = cv2.VideoCapture(0)
 
 
-ep_robot = robot.Robot()
-ep_robot.initialize(conn_type="ap")
-ep_camera = ep_robot.camera
-ep_camera.start_video_stream(display=False, resolution=camera.STREAM_360P)
+# ep_robot = robot.Robot()
+# ep_robot.initialize(conn_type="ap")
+# ep_camera = ep_robot.camera
+# ep_camera.start_video_stream(display=False, resolution=camera.STREAM_360P)
 
 
 while True:
-    # ret, frame = vid.read()
-    frame = ep_camera.read_cv2_image(strategy="newest", timeout=0.5)
+    ret, frame = vid.read()
+    # frame = ep_camera.read_cv2_image(strategy="newest", timeout=0.5)
     if frame is not None:
         start = time.time()
         if model.predictor:
