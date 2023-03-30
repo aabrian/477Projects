f560103c2d98a3ae1cd18145500417f5bc4fb9a1
Jared <jscheff11@comcast.net>
2023-03-30 17:06:02 -0400

    .

--- Proj2/example_com_reciever.py
+++ Proj2/example_com_reciever.py
@@ -1,13 +1,13 @@
 import time
 import zmq
 import time
-import zmq
-import robomaster
-from robomaster import robot
-from robomaster import config
+# import zmq
+# import robomaster
+# from robomaster import robot
+# from robomaster import config
 
-ep_robot = robot.Robot()
-ep_robot.initialize(conn_type="sta", sn="3JKCH8800100TY")
+# ep_robot = robot.Robot()
+# ep_robot.initialize(conn_type="sta", sn="3JKCH8800100TY")
 
 context = zmq.Context()
 socket = context.socket(zmq.REP)
@@ -16,10 +16,10 @@ socket.bind("tcp://*:5555")
 while True:
     #  Wait for next request from client
     message = socket.recv()
-    print("Received request: %s" % message)
+    print(f"Recieved requst: {message}")
 
     #  Do some 'work'
     time.sleep(1)
 
     #  Send reply back to client
-    socket.send(b"World")
+    socket.send_string("World")
