6b0a474d418c09e1f8416c8e417bb48999cf96c3
David <daderinw@umd.edu>
2023-04-03 15:04:27 -0400

    g

--- Proj2/example_com_sender.py
+++ Proj2/example_com_sender.py
@@ -1,28 +1,19 @@
-import zmq
 import time
 import zmq
-import robomaster
-from robomaster import robot
-from robomaster import config
-
-context = zmq.Context()
-socket = context.socket(zmq.REQ)
-socket.connect("tcp://192.168.50.134:5555") # figure out IP adress stuff
-ep_robot = robot.Robot()
-ep_robot.initialize(conn_type="sta",sn = "3JKCH8800100WV")
-
-context = zmq.Context()
 
 #  Socket to talk to server
-print("Connecting to hello world server…")
+context = zmq.Context()
 socket = context.socket(zmq.REQ)
-socket.connect("tcp://192.168.50.134:5555")
+socket.connect("tcp://10.104.17.232:5555")
+print("Connecting to hello world server…")
+
 
 #  Do 10 requests, waiting each time for a response
+
 for request in range(10):
-    print("Sending request %s …" % request)
-    socket.send(b"Hello")
+    print(f"Sending request {request} ...")
+    socket.send_string("Hello")
 
     #  Get the reply.
     message = socket.recv()
-    print("Received reply %s [ %s ]" % (request, message))
+    print(f"Received reply {request} [ {message} ]")
