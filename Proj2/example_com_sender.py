8d60430c9531f9ec0d1695bbd1ff8c2dc9cfce4a
David <daderinw@umd.edu>
2023-04-17 15:23:19 -0400

    gs

--- Proj2/example_com_sender.py
+++ Proj2/example_com_sender.py
@@ -8,7 +8,8 @@ from robomaster import config
 #  Socket to talk to server
 context = zmq.Context()
 socket = context.socket(zmq.REQ)
-socket.connect("tcp://192.168.50.134:5555")
+#  you want to use the IP adress of thr computer connected to the robot you want to talk too
+socket.connect("tcp://192.168.50.90:5555")
 print("Connecting to ROBOT2…")
 
 
